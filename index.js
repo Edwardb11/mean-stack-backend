499d3efb6a75239e904e0cf8658547fee8ed4674
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-31 13:20:03 -0400

    configurando cor para el error

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const cors = require("cors")
 const connexionDB = require("./db/config");
 const authRouter = require("./routes/auth");
 const app = express();
 // Para usar variables de entorno
 require("dotenv").config();
 // Conectado a la BD
-connexionDB()
-app.use(express.json())
+connexionDB();
+app.use(express.json());
+app.use(cors());
 
 app.use("/", express.static(__dirname + "/public"));
 
-app.use("/auth",authRouter)
+app.use("/auth", authRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Aplicacion corriendo en el puerto ${process.env.PORT} `);
