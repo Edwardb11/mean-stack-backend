3ef58b28571c0fe388a8217b95c40de3953ef090
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-31 10:37:54 -0400

    archivos estaticos

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const express = require("express");
 const app = express();
-app.get("/",(req,res)=>{
-  res.send("Acceso")
-})
-app.listen(3000,()=>{
-  console.log("Aplicacion corriendo en el puerto 3000 ")
-})
+app.use("/", express.static(__dirname + "/public"));
+// app.get("/", (req, res) => {
+//   res.send("Acceso");
+// });
+app.listen(3000, () => {
+  console.log("Aplicacion corriendo en el puerto 3000 ");
+});
