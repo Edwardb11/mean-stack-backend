61a200c4304bbcddb2fa08f9888445f5178548d0
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-31 11:40:40 -0400

    schema y modulos de mongodb

---
+++ models/usuario.js
@@ -0,0 +1,23 @@
+
+const { Schema, model } = require("mongoose")
+
+// Estructura que va a tener los usuarios en las coleciones de mongoDB Schema
+const usuario = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    unique: true,
+    type: String,
+    required: true
+
+  },
+  password: {
+    type: String,
+    required: true
+  },
+})
+
+// tienes quye exportar con model de mongoose
+module.exports = model("usuario", usuario)
