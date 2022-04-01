de5df8b0a99103a5f7561b1f8f00f4a12d7efbb1
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-04-01 10:21:47 -0400

    endpoint para actualizar

--- controllers/tareaControllers.js
+++ controllers/tareaControllers.js
@@ -1,6 +1,7 @@
 const tarea = require("../models/tarea");
 const Tarea = require("../models/tarea");
 
+// Funcion para crear una tarea POST
 const createTarea = async (req, res) => {
   const { nombre } = req.body;
   const id = req.uid;
@@ -17,6 +18,8 @@ const createTarea = async (req, res) => {
     tarea,
   });
 };
+
+// Funcion para leer una tarea GET
 const readTarea = async (req, res) => {
   const id = req.uid;
   try {
@@ -33,4 +36,23 @@ const readTarea = async (req, res) => {
   }
 };
 
-module.exports = { createTarea, readTarea };
+// Funcion para actualizar una tarea PUT
+const updateTarea = async (req, res) => {
+  // Obtener el id de la tarea
+  const { id } = req.params;
+  const { nombre } = req.body;
+  try {
+    const tarea = await Tarea.findByIdAndUpdate(id, { nombre },{new:true});
+    return res.json({
+      ok: true,
+      tarea,
+    });
+  } catch (error) {
+    res.status(404).json({
+      ok: false,
+      msg: "Tarea no actualizada",
+    });
+  }
+};
+
+module.exports = { createTarea, readTarea, updateTarea };
--- routes/tareas.js
+++ routes/tareas.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
-const { createTarea } = require("../controllers/tareaControllers");
-const { readTarea } = require("../controllers/tareaControllers");
+const { createTarea,readTarea,updateTarea } = require("../controllers/tareaControllers");
 const verifyToken = require("../middlewares/verifyToken");
+
 
 const router = new Router();
 
 router.post("/create", [verifyToken], createTarea);
 router.get("/read", [verifyToken], readTarea);
+router.put("/update/:id", [verifyToken], updateTarea);
 
 module.exports = router;
